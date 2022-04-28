50cc2bb2cd8fe38335631a7b9c279a40996be5ad
Rushikesh Patil <rushipatil6632@gmail.com>
2022-04-28 19:56:35 +0530

    Added:use instance variable

--- Employeewage/Empwage.php
+++ Employeewage/Empwage.php
@@ -3,19 +3,25 @@ class EmployeeWage{
     function welcome(){
         echo "Welcome to employeewage";
     }
-    public $WAGE_PER_HR = 20;
     public $FULL_TIME_WORKING_HRS = 8;
     public $PART_TIME_WORKING_HRS = 4;
     public $IS_FILL_TIME = 2;
     public $IS_PART_TIME = 1;
     public $IS_ABSENT = 0;
-    public $WORKING_DAYS_PER_MONTH = 20;
-    public $WORKING_HOURS_PER_MONTH = 100;
+    public $WAGE_PER_HR;
+    public $WORKING_DAYS_PER_MONTH;
+    public $WORKING_HOURS_PER_MONTH;
     public $workingHrs = 0;
     public $monthlyWage = 0;
     public $totalWorkingDays = 0;
     public $totalWorkingHours = 0;
 
+    public function __construct($wage, $days, $hours)
+    {
+        $this->WAGE_PER_HR = $wage;
+        $this->WORKING_DAYS_PER_MONTH = $days;
+        $this->WORKING_HOURS_PER_MONTH = $hours;
+    }
     /**
      * Function to Check Employee is Present or Absent
      * Returns working hrs
@@ -43,7 +49,6 @@ class EmployeeWage{
 
     /**
      * Function to Calculate Daily Wage
-     * Non-Parameterized Function
      * Printing the daily wage to the output
      * Calling attendance function to check employee attendance
      * returns daily wage of the employee
@@ -71,7 +76,7 @@ class EmployeeWage{
         ) {
             $this->totalWorkingDays++;
             echo "Day:: " . $this->totalWorkingDays . "\n";
-            $dailyWage = $this->dailyWage();
+            $dailyWage = $this->dailyWage($this->WAGE_PER_HR);
             $this->monthlyWage += $dailyWage;
             $this->totalWorkingHours += $this->workingHrs;
         }
@@ -80,16 +85,19 @@ class EmployeeWage{
         echo "Total Working Hours:: " . $this->totalWorkingHours . "\n";
         echo "Monthly Wage:: " . $this->monthlyWage . "\n\n";
     }
+
+    /**
+     * Function to take user input for wage per hour, max working days and 
+     * max working hours, calling monthly wage function and
+     * passing these constant variables as parameters
+     */
     function addCompany()
     {
         $name = readline('Enter Name of Company: ');
         echo "Employee Wage Computation For\n";
-        $WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
-        $WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
-        $WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
-        $this->monthlyWage($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);
+        $this->monthlyWage();
     }
 }
-$company = new EmployeeWage();
-$company->addCompany();
+$company1 = new EmployeeWage(20, 20, 100);
+$company1->addCompany();
 ?>
