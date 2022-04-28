9bdb728bc1a7db529d423d2c7c0e3ae14baa7e3d
Rushikesh Patil <rushipatil6632@gmail.com>
2022-04-28 19:13:42 +0530

    Added:Checking the attendance of the employee

--- Employeewage/Empwage.php
+++ Employeewage/Empwage.php
@@ -3,7 +3,21 @@ class EmployeeWage{
     function welcome(){
         echo "Welcome to employeewage";
     }
+/**
+     * Function to Check Employee is Present or Absent
+     * Non-Parameterized function
+     * Using rand() to generate the random of 0 or 1
+     */
+    function attendance()
+    {
+        $empCheck = rand(0, 1);
+        if ($empCheck == 1) {
+            echo "\nEmployee is Present";
+        } else {
+            echo "\nEmployee is Absent";
+        }
+    }
 }
 $employeeWage = new EmployeeWage();
-$employeeWage->welcome();
+$employeeWage->attendance();
 ?>
