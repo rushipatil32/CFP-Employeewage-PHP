d8055a1a62ed7090172926dafcf1862867d3415c
Rushikesh Patil <rushipatil6632@gmail.com>
2022-04-28 19:32:20 +0530

    Added:calculate monthly wage of the employee

--- Employeewage/Empwage.php
+++ Employeewage/Empwage.php
@@ -9,6 +9,7 @@ class EmployeeWage{
     public $IS_FILL_TIME = 2;
     public $IS_PART_TIME = 1;
     public $IS_ABSENT = 0;
+    public $WORKING_DAYS_PER_MONTH = 20;
 
     /**
      * Function to Check Employee is Present or Absent
@@ -39,14 +40,35 @@ class EmployeeWage{
      * Function to Calculate Daily Wage
      * Non-Parameterized Function
      * Printing the daily wage to the output
+     * Calling attendance function to check employee attendance
+     * returns daily wage of the employee
      */
     function dailyWage()
     {
         $hrs = $this->attendance();
         $dailyWage = $this->WAGE_PER_HR * $hrs;
-        echo "Daily Wage: " . $dailyWage;
+        echo "Working Hours:: " . $hrs . "\n";
+        echo "Daily Wage:: " . $dailyWage . "\n\n";
+        return $dailyWage;
+    }
+
+    /**
+     * Function to Calculate Monthly Wage
+     * Non-Parameterized Function
+     * Printing the Monthly wage to the output
+     * Calling daily wage function to get daily wage
+     */
+    function monthlyWage()
+    {
+        $monthlyWage = 0;
+        for ($i = 1; $i <= $this->WORKING_DAYS_PER_MONTH; $i++) {
+            echo "Day:: " . $i . "\n";
+            $dailyWage = $this->dailyWage();
+            $monthlyWage += $dailyWage;
+        }
+        echo "Monthly Wage:: " . $monthlyWage . "\n\n";
     }
 }
 $employeeWage = new EmployeeWage();
-$employeeWage->dailyWage();
+$employeeWage->monthlyWage();
 ?>
